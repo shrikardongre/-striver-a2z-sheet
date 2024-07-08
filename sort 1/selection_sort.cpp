ec86c7998bcb4fbe982d750c6b6b464e0f83347f
shrikardongre <shrikardongre@gmail.com>
2024-07-08 13:06:35 +0530

    done

--- sort 1/selection_sort.cpp
+++ sort 1/selection_sort.cpp
@@ -3,23 +3,25 @@ using namespace std;
 void selection_sort(int arr[],int n){
     for (int i =0;i<n;i++){
         int min=i;
-        for(int j=i;j<n;j++){
+        for(int j=i+1;j<n;j++){
             if (arr[j]<arr[min]){
                  min=j;
             }
-                 swap(arr[min],arr[i]);
-            
+                    
+  }
+            int temp = arr[min];
+                    arr[min] = arr[i];
+                     arr[i] = temp;
         }
-
-    }
-    for (int i=0;i<n;i++){
+         for (int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-}
+    }
+    
 
 int main()
 {
-    int a[]={1,5,3,7,9};
+    int a[]={4,1,3,7,9};
     selection_sort(a,5);
     return 0;
 }
